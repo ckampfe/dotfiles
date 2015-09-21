017ad435d5948777e8bd97f04d7161547d8c5ce9
Clark Kampfe <clark@bellycard.com>
2015-09-21 16:35:05 -0500

    ammonite - math, scalatags, scalacsv

--- predef.scala
+++ predef.scala
@@ -6,9 +6,9 @@ import ammonite.ops._
 import ammonite.ops.ImplicitWd._
 repl.prompt.bind(s"[${cwd.toString}]@ ")
 
-// @ import scala.math._
+@ import scala.math._
 
-// @ load.ivy("com.lihaoyi" %% "scalatags" % "0.5.2")
-// @ load.ivy("com.github.tototoshi" %% "scala-csv" % "1.2.2")
+@ load.ivy("com.lihaoyi" %% "scalatags" % "0.5.2")
+@ load.ivy("com.github.tototoshi" %% "scala-csv" % "1.2.2")
 // @ load.ivy("org.json4s" %% "json4s-jackson" % "3.2.11")
-// @ load.ivy("org.scalaj" %% "scalaj-http" % "1.1.5")
+@ load.ivy("org.scalaj" %% "scalaj-http" % "1.1.5")
