3689c9c849125811c7652d35f80c5a204c4419d2
Clark Kampfe <clark@bellycard.com>
2016-02-22 17:59:14 -0600

    initial clj ~/.lein/profiles.clj

---
+++ profiles.clj
@@ -0,0 +1,10 @@
+{:repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]}
+ :user {:plugins [[cider/cider-nrepl "0.10.2"]
+                  [org.clojure/tools.nrepl "0.2.12"]
+                  [refactor-nrepl "2.0.0"]
+                  [lein-exec "0.3.6"]
+                  ]
+        :dependencies [[org.clojure/clojure "1.8.0"]]
+        :dev {:dependencies [[org.clojure/tools.nrepl "0.2.12"]] }
+        :mirrors {#"clojars" {:name "clojars mirror"
+                              :url "https://clojars-mirror.tcrawley.org/repo/"}}}}
