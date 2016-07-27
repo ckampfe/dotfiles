d4d8d424106937f89b562f4dcfdfa52fd35ee687
Clark Kampfe <clark@bellycard.com>
2016-07-27 10:59:48 -0600

    profiles.clj bump clojure version

--- profiles.clj
+++ profiles.clj
@@ -4,7 +4,7 @@
                   [refactor-nrepl "2.2.0"]
                   [lein-exec "0.3.6"]
                   ]
-        :dependencies [[org.clojure/clojure "1.8.0"]]
+        :dependencies [[org.clojure/clojure "1.9.0-alpha10"]]
         :dev {:dependencies [[org.clojure/tools.nrepl "0.2.12"]] }
         :mirrors {#"clojars" {:name "clojars mirror"
                               :url "https://clojars-mirror.tcrawley.org/repo/"}}}}
